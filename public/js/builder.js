3cf3c88ff6844da8c35b68ec5c469a0fec6ba017
Tilo Mitra <tilomitra@gmail.com>
2012-07-31 11:40:59 -0700

    minor refactoring of scripts. in-page script now in builder.js

---
+++ public/js/builder.js
@@ -0,0 +1,49 @@
+        YUI({
+            modules: {
+                cssView: {
+                    fullpath: '/public/js/views/CSSView.js'
+                },
+                htmlView: {
+                    fullpath: '/public/js/views/HTMLView.js'
+                },
+                
+                controlView: {
+                    fullpath: '/public/js/views/ControlView.js'
+                },
+                
+                widthView: {
+                    fullpath: '/public/js/views/WidthView.js'
+                },
+                
+                gridModel: {
+                    fullpath: '/public/js/models/GridModel.js'
+                },
+                
+            }
+        }).use('app', 'cssView', 'htmlView', 'controlView', 'widthView', 'gridModel', function (Y) {
+
+            var app = new Y.App({
+                container    : '#wrapper',
+                viewContainer: '#wrapper'
+            });
+
+            app.render();
+
+            //Instantiate Shared Grid Model
+            var gridModel = new Y.GB.GridModel({ width: 960 });
+
+            //Instantiate Control View to manipulate model attributes.     
+            var controlView = new Y.GB.ControlView({ model: gridModel });
+            controlView.render();
+
+            //Instantiate CSS View and Render
+            var cssView = new Y.GB.CSSView({ model: gridModel });
+            cssView.render();
+
+            //Instantiate HTML View and Render
+            var htmlView = new Y.GB.HTMLView({ model: gridModel });
+            htmlView.render();
+
+            //Track Screen Size
+            var screenSizeDisplayer = new Y.GB.WidthView();
+        });
